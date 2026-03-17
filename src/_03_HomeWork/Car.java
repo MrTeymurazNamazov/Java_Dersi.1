bbefe4656cdfdcf324a9c2280ba062706f655a00
Teymuraz <mr.teymurnamazov@gmail.com>
2026-03-17 20:00:24 +0400

    hazirdi

---
+++ src/_03_HomeWork/Car.java
@@ -0,0 +1,21 @@
+package _03_HomeWork;
+
+public class Car {
+    String marka;
+    String model;
+    String color;
+    Double motorsize;
+    int doors;
+    public void print(){
+        System.out.println("Marka: "+marka);
+        System.out.println("Model: "+model);
+        System.out.println("Color: "+color);
+        System.out.println("Motorsize: "+motorsize);
+        System.out.println("Doors: "+doors);
+    }
+
+}
+
+
+
+
---
+++ src/_03_HomeWork/OOP_question.java
@@ -0,0 +1,14 @@
+package _03_HomeWork;
+
+public class OOP_question {
+    static void main() {
+        Car car1 = new Car();
+        car1.marka= "Bmw";
+        car1.model= "E39";
+       car1.color = "red";
+       car1.motorsize=2.5;
+       car1.doors=4;
+       car1.print();
+    }
+
+}
