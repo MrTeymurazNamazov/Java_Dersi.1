d1a9bb9aea5e9fdb650a646de2d78fc4d4d6618f
Teymuraz <mr.teymurnamazov@gmail.com>
2026-03-18 21:31:21 +0400

    hazirdi

--- src/_03_HomeWork/Car.java
+++ src/_03_HomeWork/Car.java
@@ -1,18 +1,72 @@
 package _03_HomeWork;
 
 public class Car {
-    String marka;
-    String model;
-    String color;
-    Double motorsize;
-    int doors;
-    public void print(){
-        System.out.println("Marka: "+marka);
-        System.out.println("Model: "+model);
-        System.out.println("Color: "+color);
-        System.out.println("Motorsize: "+motorsize);
-        System.out.println("Doors: "+doors);
+    private String marka;
+   private String model;
+    private String color;
+    private Double motorsize;
+    private int doors;
+     Car(String marka, String model, String color, Double motorsize, int doors) {
+        this.marka = marka;
+        this.model = model;
+        this.color = color;
+        this.motorsize = motorsize;
+        this.doors = doors;
+
     }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getMotorsize() {
+        return motorsize;
+    }
+
+    public void setMotorsize(Double motorsize) {
+        this.motorsize = motorsize;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                " Marka = '" + marka + '\'' +
+                ", Model = '" + model + '\'' +
+                ", Color = '" + color + '\'' +
+                ", Motorsize = " + motorsize +
+                ", Doors = " + doors +
+                '}';
+    }
+
+
 
 }
 
---
+++ src/_03_HomeWork/CarMain.java
@@ -0,0 +1,11 @@
+package _03_HomeWork;
+
+public class CarMain {
+    static void main() {
+        Car car1 = new Car("Bmw","E39","black",2.5,4);
+        Car car2 = new Car("Bmw","E60","white",3.0,4);
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+
+}
